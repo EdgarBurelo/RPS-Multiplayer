cc933a2db559e98ac0db9b3f207d1ffc5d17f8cd
Edgar Burelo <edgar.burelo@gmail.com>
2018-09-17 14:26:32 -0500

    create folders and files, link Firebase, jquery, moment, etc

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyChanswKw6AnZYI3B6lec7bLiMIrc_l1P4",
+    authDomain: "rps-multiplayer-51613.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-51613.firebaseio.com",
+    projectId: "rps-multiplayer-51613",
+    storageBucket: "",
+    messagingSenderId: "476067623389"
+  };
+  firebase.initializeApp(config);
